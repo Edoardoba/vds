e6da6cf4025a043c645d276f5e649869cf4067e4
Edoardo Cantagallo <cantagalloedoardo@gmail.com>
2025-10-29 16:55:52 +0100

    a

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -30,5 +31,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         },
       }}
     />
+    <Analytics />
   </React.StrictMode>,
 )
